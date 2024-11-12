4db1087265adc4a169628a6d787880abf3b81989
Mathias Weiden <mtweiden@berkeley.edu>
2024-11-12 12:27:16 -0800

    Added bounding_box method for Ellipses

--- tests/test_ellipse.py
+++ tests/test_ellipse.py
@@ -2,6 +2,7 @@ from random import random
 
 from numpy import array
 from numpy.linalg import eigvals
+from numpy import isclose
 
 from cyclosynth.ellipse import Ellipse
 
@@ -9,13 +10,14 @@ from cyclosynth.ellipse import Ellipse
 from random import seed
 seed(42)
 
-def random_ellipse() -> Ellipse:
+def random_ellipse(random_center: bool = False) -> Ellipse:
     a, b, d = 1000 * random(), 1000 * random(), 1000 * random()
     mat = array([[a, b], [b, d]])
     mat = (mat @ mat.T)
     assert all(eigvals(mat) > 0)
     a, b, d = mat[0, 0], mat[0, 1], mat[1, 1]
-    return Ellipse([a, b, d])
+    center = (random() * 1000, random() * 1000) if random_center else (0, 0)
+    return Ellipse([a, b, d], center)
 
 
 class TestEllipse:
@@ -41,3 +43,35 @@ class TestEllipse:
             manual_e = e.copy()
             manual_e = manual_e.apply_operator(op)
             assert new_e.is_close(manual_e)
+    
+    def test_bounding_box(self) -> None:
+        for _ in range(self.num_trials):
+            e = random_ellipse(random_center=True)
+            (x_lo, x_hi), (y_lo, y_hi) = e.bounding_box()
+            assert x_lo <= x_hi
+            assert y_lo <= y_hi
+
+            p1, p2 = e.center
+            a, b, d = e.a, e.b, e.d
+
+            def descriminant_x_bounds(x: float) -> float:
+                beta = (2 * p2 * d - 2 * b * x) / d
+                alpha = (a * x ** 2 - 2 * b * x * p2 + d * p2 ** 2 - 1) / d
+                return beta ** 2 - 4 * alpha
+            
+            def descriminant_y_bounds(y: float) -> float:
+                beta = (2 * p1 * a - 2 * b * y) / a
+                alpha = (a * p1 ** 2 - 2 * b * p1 * y + d * y ** 2 - 1) / a
+                return beta ** 2 - 4 * alpha
+
+            # Test x bounds
+            desc_x_lo = descriminant_x_bounds(x_lo - p1)
+            desc_x_hi = descriminant_x_bounds(x_hi - p1)
+            assert isclose(desc_x_lo, 0, atol=1e-3)
+            assert isclose(desc_x_hi, 0, atol=1e-3)
+
+            # Test y bounds
+            desc_y_lo = descriminant_y_bounds(y_lo - p2)
+            desc_y_hi = descriminant_y_bounds(y_hi - p2)
+            assert isclose(desc_y_lo, 0, atol=1e-3)
+            assert isclose(desc_y_hi, 0, atol=1e-3)
