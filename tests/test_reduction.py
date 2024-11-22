89f7cfcb34c4aa8d3fa8400d9d99640153012f86
Mathias Weiden <mtweiden@berkeley.edu>
2024-11-22 14:43:54 -0800

    Working recursive step_lemma implementation

--- tests/test_reduction.py
+++ tests/test_reduction.py
@@ -12,7 +12,7 @@ from cyclosynth.ellipse import Ellipse
 from cyclosynth.reduction import apply_op
 from cyclosynth.reduction import bias
 from cyclosynth.reduction import skew
-from cyclosynth.reduction import shift_k
+from cyclosynth.reduction import shift_k_ellipses
 from cyclosynth.reduction import R_lemma
 from cyclosynth.reduction import K_lemma
 from cyclosynth.reduction import A_lemma
@@ -56,7 +56,6 @@ def random_state(
     return ell1, ell2
 
 
-
 class TestReduction:
 
     num_trials = 100
@@ -66,7 +65,7 @@ class TestReduction:
             sign = 1 if randint(0, 1) == 0 else -1
             bias_ = sign * uniform(2.0, 10.0)
             ell1, ell2 = random_state(bias=bias_)
-            assert isclose(bias(ell1, ell2), bias_, atol=1e-5)
+            assert isclose(bias(ell1, ell2), bias_, rtol=1e-5)
 
     def test_shift_k(self) -> None:
         # Bias(D, Delta) > 1 -> zeta > 1 + z
@@ -79,21 +78,23 @@ class TestReduction:
             bias_1 = float(bias(ell1, ell2))
             skew_1 = float(skew(ell1, ell2))
             # Apply operator
-            ella, ellb = shift_k(ell1, ell2, -sign * k, False)
+            ella, ellb = shift_k_ellipses(ell1, ell2, -sign * k)
             bias_2 = float(bias(ella, ellb))
             skew_2 = float(skew(ella, ellb))
-            assert isclose(skew_1, skew_2, atol=1e-5)
-            assert isclose(bias_2 + sign * 2 * k, bias_1, atol=1e-5)
-            # Manually check operator
-            _, _, sigmak, tauk = shift_k(ell1, ell2, -sign * k, True)
-            ellaa = apply_op(ell1, sigmak)
-            ellbb = apply_op(ell2, tauk)
-            assert isclose(float(ellaa.a), float(ella.a), atol=1e-5)
-            assert isclose(float(ellaa.b), float(ella.b), atol=1e-5)
-            assert isclose(float(ellaa.d), float(ella.d), atol=1e-5)
-            assert isclose(float(ellbb.a), float(ellb.a), atol=1e-5)
-            assert isclose(float(ellbb.b), float(ellb.b), atol=1e-5)
-            assert isclose(float(ellbb.d), float(ellb.d), atol=1e-5)
+            assert isclose(skew_1, skew_2, rtol=1e-5)
+            assert isclose(bias_2 + sign * 2 * k, bias_1, rtol=1e-5)
+            # Comparing to grid operator requires shifting back
+            ellaa, ellbb = shift_k_ellipses(ella, ellb, sign * k)
+            bias_3 = float(bias(ellaa, ellbb))
+            skew_3 = float(skew(ellaa, ellbb))
+            assert isclose(skew_1, skew_3, rtol=1e-5)
+            assert isclose(bias_3, bias_1, rtol=1e-5)
+            assert(float(ell1.a) == float(ellaa.a))
+            assert(float(ell1.b) == float(ellaa.b))
+            assert(float(ell1.d) == float(ellaa.d))
+            assert(float(ell2.a) == float(ellbb.a))
+            assert(float(ell2.b) == float(ellbb.b))
+            assert(float(ell2.d) == float(ellbb.d))
     
     def test_R_lemma(self) -> None:
         for _ in range(self.num_trials):
@@ -103,19 +104,18 @@ class TestReduction:
             ell1, ell2 = random_state(z=z, zeta=zeta)
             skew1 = float(skew(ell1, ell2))
             # Apply operator
-            ella, ellb = R_lemma(ell1, ell2, False)
+            ella, ellb, R, R_bul = R_lemma(ell1, ell2)
             skew2 = float(skew(ella, ellb))
             assert skew2 <= 0.9 * skew1
             # Manually check operator
-            _, _, R, R_bul = R_lemma(ell1, ell2, True)
             ellaa = apply_op(ell1, R)
             ellbb = apply_op(ell2, R_bul)
-            assert isclose(float(ellaa.a), float(ella.a), atol=1e-5)
-            assert isclose(float(ellaa.b), float(ella.b), atol=1e-5)
-            assert isclose(float(ellaa.d), float(ella.d), atol=1e-5)
-            assert isclose(float(ellbb.a), float(ellb.a), atol=1e-5)
-            assert isclose(float(ellbb.b), float(ellb.b), atol=1e-5)
-            assert isclose(float(ellbb.d), float(ellb.d), atol=1e-5)
+            assert isclose(float(ellaa.a), float(ella.a), rtol=1e-5)
+            assert isclose(float(ellaa.b), float(ella.b), rtol=1e-5)
+            assert isclose(float(ellaa.d), float(ella.d), rtol=1e-5)
+            assert isclose(float(ellbb.a), float(ellb.a), rtol=1e-5)
+            assert isclose(float(ellbb.b), float(ellb.b), rtol=1e-5)
+            assert isclose(float(ellbb.d), float(ellb.d), rtol=1e-5)
     
     def test_K_lemma(self) -> None:
         # bias(D, Delta) in [-1, 1], zeta >= 0.8 and z <= 0.3 imply that
@@ -127,19 +127,18 @@ class TestReduction:
             ell1, ell2 = random_state(z=z, zeta=zeta)
             skew1 = float(skew(ell1, ell2))
             # Apply operator
-            ella, ellb = K_lemma(ell1, ell2, False)
+            ella, ellb, K, K_bul = K_lemma(ell1, ell2)
             skew2 = float(skew(ella, ellb))
             assert skew2 <= 0.9 * skew1
             # Manually check operator
-            _, _, K, K_bul = K_lemma(ell1, ell2, True)
             ellaa = apply_op(ell1, K)
             ellbb = apply_op(ell2, K_bul)
-            assert isclose(float(ellaa.a), float(ella.a), atol=1e-5)
-            assert isclose(float(ellaa.b), float(ella.b), atol=1e-5)
-            assert isclose(float(ellaa.d), float(ella.d), atol=1e-5)
-            assert isclose(float(ellbb.a), float(ellb.a), atol=1e-5)
-            assert isclose(float(ellbb.b), float(ellb.b), atol=1e-5)
-            assert isclose(float(ellbb.d), float(ellb.d), atol=1e-5)
+            assert isclose(float(ellaa.a), float(ella.a), rtol=1e-5)
+            assert isclose(float(ellaa.b), float(ella.b), rtol=1e-5)
+            assert isclose(float(ellaa.d), float(ella.d), rtol=1e-5)
+            assert isclose(float(ellbb.a), float(ellb.a), rtol=1e-5)
+            assert isclose(float(ellbb.b), float(ellb.b), rtol=1e-5)
+            assert isclose(float(ellbb.d), float(ellb.d), rtol=1e-5)
     
     def test_A_lemma(self) -> None:
         # bias(D, Delta) in [-1, 1], and z, zeta >= 0.3
@@ -152,19 +151,18 @@ class TestReduction:
             ell1, ell2 = random_state(z=z, bias=bias_)
             skew1 = float(skew(ell1, ell2))
             # Apply operator
-            ella, ellb = A_lemma(ell1, ell2, False)
+            ella, ellb, A, A_bul = A_lemma(ell1, ell2)
             skew2 = float(skew(ella, ellb))
             assert skew2 <= 0.9 * skew1
             # Manually check operator
-            _, _, A, A_bul = A_lemma(ell1, ell2, True)
             ellaa = apply_op(ell1, A)
             ellbb = apply_op(ell2, A_bul)
-            assert isclose(float(ellaa.a), float(ella.a), atol=1e-5)
-            assert isclose(float(ellaa.b), float(ella.b), atol=1e-5)
-            assert isclose(float(ellaa.d), float(ella.d), atol=1e-5)
-            assert isclose(float(ellbb.a), float(ellb.a), atol=1e-5)
-            assert isclose(float(ellbb.b), float(ellb.b), atol=1e-5)
-            assert isclose(float(ellbb.d), float(ellb.d), atol=1e-5)
+            assert isclose(float(ellaa.a), float(ella.a), rtol=1e-5)
+            assert isclose(float(ellaa.b), float(ella.b), rtol=1e-5)
+            assert isclose(float(ellaa.d), float(ella.d), rtol=1e-5)
+            assert isclose(float(ellbb.a), float(ellb.a), rtol=1e-5)
+            assert isclose(float(ellbb.b), float(ellb.b), rtol=1e-5)
+            assert isclose(float(ellbb.d), float(ellb.d), rtol=1e-5)
     
     def test_B_lemma(self) -> None:
         # bias(D, Delta) in [-1, 1], b <= 0 <= beta, and z, zeta >= -0.2
@@ -177,45 +175,35 @@ class TestReduction:
             ell1, ell2 = random_state(z=z, bias=bias_, b_sign=-1)
             skew1 = float(skew(ell1, ell2))
             # Apply operator
-            ella, ellb = B_lemma(ell1, ell2, False)
+            ella, ellb, B, B_bul = B_lemma(ell1, ell2)
             skew2 = float(skew(ella, ellb))
             assert skew2 <= 0.9 * skew1
             # Manually check operator
-            _, _, B, B_bul = B_lemma(ell1, ell2, True)
             ellaa = apply_op(ell1, B)
             ellbb = apply_op(ell2, B_bul)
-            assert isclose(float(ellaa.a), float(ella.a), atol=1e-5)
-            assert isclose(float(ellaa.b), float(ella.b), atol=1e-5)
-            assert isclose(float(ellaa.d), float(ella.d), atol=1e-5)
-            assert isclose(float(ellbb.a), float(ellb.a), atol=1e-5)
-            assert isclose(float(ellbb.b), float(ellb.b), atol=1e-5)
-            assert isclose(float(ellbb.d), float(ellb.d), atol=1e-5)
+            assert isclose(float(ellaa.a), float(ella.a), rtol=1e-5)
+            assert isclose(float(ellaa.b), float(ella.b), rtol=1e-5)
+            assert isclose(float(ellaa.d), float(ella.d), rtol=1e-5)
+            assert isclose(float(ellbb.a), float(ellb.a), rtol=1e-5)
+            assert isclose(float(ellbb.b), float(ellb.b), rtol=1e-5)
+            assert isclose(float(ellbb.d), float(ellb.d), rtol=1e-5)
     
     def test_reduce(self) -> None:
         for _ in range(self.num_trials):
             # Set up
             angle = uniform(0, 2 * pi)
             epsilon = 1e-5
-            ell1 = Ellipse.find_ellipse(angle, epsilon)
+            ell = Ellipse.find_ellipse(angle, epsilon)
+            disc = Ellipse([1, 0, 1], (0, 0))
 
-            # Apply reduction
-            ell2, disc, opell, opdisc = reduce(ell1)
-            norm = sqrt(ell2.det())
-            ell2_normalized = Ellipse(
-                [ell2.a / norm, ell2.b / norm, ell2.d / norm],
-                ell2.center,
+            norm = sqrt(ell.det())
+            ell_norm = Ellipse(
+                [ell.a / norm, ell.b / norm, ell.d / norm],
+                ell.center,
             )
-            assert ell2_normalized.skew() < 15
+            # Apply reduction
+            opG = reduce(ell_norm, disc)
+            # opG = old_reduce(ell)
+            ell_norm = apply_op(ell_norm, opG)
+            assert ell_norm.skew() < 15
             assert disc.skew() < 15
-
-            # Manually check operators
-            ell_manual = apply_op(ell1, opell)
-            disc_manual = apply_op(Ellipse([1, 0, 1]), opdisc)
-
-            assert isclose(float(disc_manual.a), float(disc.a), atol=1e-5)
-            assert isclose(float(disc_manual.b), float(disc.b), atol=1e-5)
-            assert isclose(float(disc_manual.d), float(disc.d), atol=1e-5)
-
-            assert isclose(float(ell_manual.a), float(ell2.a), atol=1e-5)
-            assert isclose(float(ell_manual.b), float(ell2.b), atol=1e-5)
-            assert isclose(float(ell_manual.d), float(ell2.d), atol=1e-5)
