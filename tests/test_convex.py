5e8ff969014ee26b76b4cf5fb0e549a34e9b9cbd
Mathias Weiden <mtweiden@berkeley.edu>
2024-11-26 21:46:35 -0800

    Rewrite of ConvexSet

--- tests/test_convex.py
+++ tests/test_convex.py
@@ -1,107 +1,76 @@
 from random import random
+from random import randint
 
 from mpmath import pi
 
 from numpy import isclose
 
+from cyclosynth.algebra import RingRoot2
+from cyclosynth.ratio import IntegerRatio
 from cyclosynth.convex import ConvexSet
 from cyclosynth.ellipse import Ellipse
+from cyclosynth.matrix import Vector
 
 
 from random import seed
 seed(42)
 
 
+def random_ratios(n: int) -> list[IntegerRatio]:
+    ratios = []
+    d = randint(-1000000, 1000000)
+    for _ in range(n):
+        a, b = [randint(-1000000, 1000000) for _ in range(2)]
+        _a, _b = RingRoot2([a, d]), RingRoot2([b, d])
+        ratios.append(IntegerRatio(_a, _b))
+    return ratios
+
+
 class TestConvex:
 
     num_trials = 1000
 
-    def test_line_intersection(self) -> None:
+    def test_intersection(self) -> None:
+        one = RingRoot2([1, 0])
+        zero = RingRoot2([0, 0])
+
+        def assert_intersection(sec: tuple[float], expected: bool) -> None:
+            if sec is None:
+                assert not expected
+            else:
+                assert expected
+
         for _ in range(self.num_trials):
-            angle = 2 * pi * random()
-            precision = 1e-3
-            convex = ConvexSet(angle, precision)
+            x_lo, x_hi, y_lo, y_hi = random_ratios(4)
+            if x_lo.to_float() > x_hi.to_float():
+                x_lo, x_hi = x_hi, x_lo
+            if y_lo.to_float() > y_hi.to_float():
+                y_lo, y_hi = y_hi, y_lo
 
-            (x_lo, x_hi), (y_lo, y_hi) = convex.bounding_box()
+            convex = ConvexSet.from_bounding_box((x_lo, x_hi), (y_lo, y_hi))
 
-            m = (y_hi - y_lo) / (x_hi - x_lo)
-            eps = 0.1 * abs(y_hi - y_lo)
+            hor_length = x_hi - x_lo
+            ver_length = y_hi - y_lo
 
-            def assert_intersection(
-                intersection: tuple[tuple[float]] | None,
-                is_included: bool,
-            ) -> None:
-                if intersection is None:
-                    assert not is_included
-                else:
-                    (x_lo_, x_hi_), (y_lo_, y_hi_) = intersection
-                    assert x_lo <= x_lo_ and x_hi_ <= x_hi
-                    assert y_lo <= y_lo_ and y_hi_ <= y_hi
-            
-            p0, p1 = convex.ellipse().center
-            half_len = (y_hi - y_lo) / 2
+            right = Vector((one, zero))
+            up = Vector((zero, one))
+            angled = Vector((hor_length, ver_length))
 
-            # Success cases
-            # case 1: left to top
-            slope = m
-            offset = p1 - slope * p0
-            intercept = offset + half_len - eps
-            intersection = convex.line_intersection(slope, intercept)
+            # p = (p0, p1) is a point on the plane
+            # v = (v0, v1) is a vector pointing in the direction of the line
+            center = Vector(((x_hi + x_lo) / 2, (y_hi + y_lo) / 2))
+            outside = center + Vector((hor_length, ver_length))
+
+            intersection = convex.intersection(right, center)
             assert_intersection(intersection, True)
-            # case 2: left to right
-            slope = eps
-            offset = p1 - slope * p0
-            intercept = offset
-            intersection = convex.line_intersection(slope, intercept)
+            intersection = convex.intersection(up, center)
             assert_intersection(intersection, True)
-            # case 3: bottom to right
-            slope = m
-            offset = p1 - slope * p0
-            intercept = offset - half_len - eps
-            intersection = convex.line_intersection(slope, intercept)
-            assert_intersection(intersection, True)
-            # case 4: top to right
-            slope = -m
-            offset = p1 - slope * p0
-            intercept = offset + half_len + eps
-            intersection = convex.line_intersection(slope, intercept)
-            assert_intersection(intersection, True)
-            # case 5: left to right
-            slope = -eps
-            offset = p1 - slope * p0
-            intercept = offset
-            intersection = convex.line_intersection(slope, intercept)
-            assert_intersection(intersection, True)
-            # case 6: right to bottom
-            slope = -m
-            offset = p1 - slope * p0
-            intercept = offset - half_len + eps
-            intersection = convex.line_intersection(slope, intercept)
+            intersection = convex.intersection(angled, center)
             assert_intersection(intersection, True)
 
-            # Failure cases
-            # case 7: above top
-            slope = m
-            offset = p1 - slope * p0
-            intercept = offset + half_len + eps
-            intersection = convex.line_intersection(slope, intercept)
+            intersection = convex.intersection(right, outside)
             assert_intersection(intersection, False)
-            # case 8: below bottom
-            slope = -m
-            offset = p1 - slope * p0
-            intercept = offset - half_len - eps
-            intersection = convex.line_intersection(slope, intercept)
+            intersection = convex.intersection(up, outside)
             assert_intersection(intersection, False)
-    
-    def test_disc_rotation(self) -> None:
-        disc = ConvexSet.from_ellipse(Ellipse([1, 0, 1]))
-        for _ in range(self.num_trials):
-            angle, precision = 2 * pi * random(), 1e-4
-            convex = ConvexSet(angle, precision)
-            convex, operator = convex.make_upright()
-            disc = disc.apply_operator(operator)
-            (x_lo, x_hi), (y_lo, y_hi) = disc.bounding_box()
-            # assert isclose(float(x_lo), -1)
-            # assert isclose(float(x_hi), 1)
-            # assert isclose(float(y_lo), -1)
-            # assert isclose(float(y_hi), 1)
+            intersection = convex.intersection(angled, outside)
+            assert_intersection(intersection, True)
