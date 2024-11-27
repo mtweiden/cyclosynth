a78c259cc509951fcac3c47d7c58c60b4a8182c9
Mathias Weiden <mtweiden@berkeley.edu>
2024-11-26 21:45:45 -0800

    Working act_on method

--- tests/test_operator.py
+++ tests/test_operator.py
@@ -3,6 +3,9 @@ from random import randint
 from cyclosynth.algebra import RingRoot2
 from cyclosynth.ratio import IntegerRatio
 from cyclosynth.operator import Operator
+from cyclosynth.matrix import Vector
+
+from numpy import isclose
 
 
 def random_integer_ratio() -> int:
@@ -18,6 +21,18 @@ def random_operator() -> Operator:
     return Operator(vals)
 
 
+def random_vector() -> Vector:
+    vals = [random_integer_ratio() for _ in range(2)]
+    return Vector(vals)
+
+
+def numeric_mat_mul(operator: Operator, vector: Vector) -> tuple[float]:
+    a, b, c, d = [float(v.to_float()) for v in operator.values]
+    x, y = [float(v.to_float()) for v in vector.values]
+    w = a * x + b * y
+    z = c * x + d * y
+    return [w, z]
+
 class TestOperator:
 
     num_trials = 100
@@ -31,3 +46,12 @@ class TestOperator:
             assert identity[0, 1].to_float() == 0
             assert identity[1, 1].to_float() == 1
             assert identity[1, 0].to_float() == 0
+
+    def test_vector_act_on(self) -> None:
+        for _ in range(self.num_trials):
+            op = random_operator()
+            vec = random_vector()
+            result = op.act_on(vec)
+            numeric = numeric_mat_mul(op, vec)
+            assert isclose(result.values[0].to_float(), numeric[0], rtol=1e-4)
+            assert isclose(result.values[1].to_float(), numeric[1], rtol=1e-4)
