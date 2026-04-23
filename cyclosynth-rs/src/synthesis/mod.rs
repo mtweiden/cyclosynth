0e769936674e4230fa29a5942eb9f724d2823846
Jaewon Kim <jwkim@berkeley.edu>
2026-04-23 11:38:15 -0700

    using paper algorithm for sppedup

--- cyclosynth-rs/src/synthesis/mod.rs
+++ cyclosynth-rs/src/synthesis/mod.rs
@@ -2,6 +2,7 @@ pub mod cliffords;
 pub mod decomposer;
 pub mod search;
 pub mod synthesizer;
+pub mod synthesizer2;
 
 pub use cliffords::{CLIFFORD_TABLE_T, apply_clifford_dagger, match_clifford};
 pub use decomposer::BlochDecomposer;
